d51c29c5be68ebc732c7ded75ada5647df873f14
Fadhli <ubaifadhli@it.student.pens.ac.id>
2021-09-08 09:35:31 +0700

    v1.0.6 - Reverted generated directory because Cucumber expects it to be there.

--- src/main/java/org/ubaifadhli/future/util/DirectoryConstant.java
+++ src/main/java/org/ubaifadhli/future/util/DirectoryConstant.java
@@ -2,7 +2,6 @@ package org.ubaifadhli.future.util;
 
 public interface DirectoryConstant {
     String BASE_TEST_RESOURCES = "src/test/resources/";
-    String BASE_TEST_CLASSES_TARGET = "target/test-classes/";
     String TEMPLATE_FEATURE_FOLDER = BASE_TEST_RESOURCES + "features-template/";
-    String GENERATED_FEATURE_FOLDER = BASE_TEST_CLASSES_TARGET + "features/";
+    String GENERATED_FEATURE_FOLDER = BASE_TEST_RESOURCES + "features/";
 }
