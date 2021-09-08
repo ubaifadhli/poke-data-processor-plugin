59a2ac13265644ebacbfdfa735da71baa0c88403
Fadhli <ubaifadhli@it.student.pens.ac.id>
2021-09-08 09:26:06 +0700

    v1.0.4 - Changed template directory to features-template.

--- src/main/java/org/ubaifadhli/future/util/DirectoryConstant.java
+++ src/main/java/org/ubaifadhli/future/util/DirectoryConstant.java
@@ -2,6 +2,6 @@ package org.ubaifadhli.future.util;
 
 public interface DirectoryConstant {
     String BASE_TEST_RESOURCES = "src/test/resources/";
-    String TEMPLATE_FEATURE_FOLDER = BASE_TEST_RESOURCES + "features/template/";
+    String TEMPLATE_FEATURE_FOLDER = BASE_TEST_RESOURCES + "features-template/";
     String GENERATED_FEATURE_FOLDER = BASE_TEST_RESOURCES + "features/";
 }
