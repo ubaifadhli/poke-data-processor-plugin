679b471731e61fec4ae7dca9479e789857bb3361
Fadhli <ubaifadhli@it.student.pens.ac.id>
2021-09-08 09:31:17 +0700

    v1.0.5 - Changed generated directory to test-classes.

--- src/main/java/org/ubaifadhli/future/util/DirectoryConstant.java
+++ src/main/java/org/ubaifadhli/future/util/DirectoryConstant.java
@@ -2,6 +2,7 @@ package org.ubaifadhli.future.util;
 
 public interface DirectoryConstant {
     String BASE_TEST_RESOURCES = "src/test/resources/";
+    String BASE_TEST_CLASSES_TARGET = "target/test-classes/";
     String TEMPLATE_FEATURE_FOLDER = BASE_TEST_RESOURCES + "features-template/";
-    String GENERATED_FEATURE_FOLDER = BASE_TEST_RESOURCES + "features/";
+    String GENERATED_FEATURE_FOLDER = BASE_TEST_CLASSES_TARGET + "features/";
 }
