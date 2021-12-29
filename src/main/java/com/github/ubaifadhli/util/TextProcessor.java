9747064d9fe0e2198fe46ead8c34c82fd9f8dc53
Fadhli Ubai <ubaifadhli@it.student.pens.ac.id>
2021-12-29 23:20:28 +0700

    v1.0.8 - Hopefully able to handle line that has another data column.

--- src/main/java/com/github/ubaifadhli/util/TextProcessor.java
+++ src/main/java/com/github/ubaifadhli/util/TextProcessor.java
@@ -8,32 +8,40 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextProcessor {
-    private static Pattern REGEX_PATTERN = Pattern.compile("[^{}]+(?=})");
+    private static Pattern DOUBLE_CURLY_BRACES_REGEX_PATTERN = Pattern.compile("\\{\\{.*?}}");
+    private static char NEW_LINE = '\n';
+
 
     public static String getProcessedText(Path path) throws IOException {
         StringBuilder featureText = new StringBuilder();
 
         Files.readAllLines(path).forEach(line -> {
-            Matcher matcher = REGEX_PATTERN.matcher(line);
+            Matcher matcher = DOUBLE_CURLY_BRACES_REGEX_PATTERN.matcher(line);
 
             if (matcher.find()) {
-                String propertyName = matcher.group().trim();
-                featureText.append(getExampleDataText(propertyName));
+                String propertyName = matcher.group()
+                        .replace('{', ' ')
+                        .replace('}', ' ')
+                        .trim();
+
+                featureText.append(getProcessedLineWithStoredData(line, propertyName));
 
             } else
-                featureText.append(line).append("\n");
+                featureText.append(line).append(NEW_LINE);
         });
 
         return featureText.toString();
     }
 
-    public static String getExampleDataText(String propertyName) {
-        List<String> exampleData = PropertiesHelper.getPropertyAsList(propertyName);
-        StringBuilder exampleDataText = new StringBuilder();
+    public static String getProcessedLineWithStoredData(String line, String propertyName) {
+        List<String> storedData = PropertiesHelper.getPropertyAsList(propertyName);
+        StringBuilder processedLines = new StringBuilder();
 
-        for (String singleData : exampleData)
-            exampleDataText.append("      | ").append(singleData).append(" |").append("\n");
+        storedData.forEach(data -> {
+            String processedLine = line.replaceFirst(DOUBLE_CURLY_BRACES_REGEX_PATTERN.pattern(), data);
+            processedLines.append(processedLine).append(NEW_LINE);
+        });
 
-        return exampleDataText.toString();
+        return processedLines.toString();
     }
 }
